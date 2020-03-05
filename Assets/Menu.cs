f59d76ee81dfeb3bbf717ad45262f29bb5b009f2
Niek Huisman <150047@csgaugustinus.nl>
2020-03-05 11:38:31 +0100

    sfsdf

---
+++ Assets/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    
+	public void Play (){
+
+		SceneManager.LaodScene(SceneManager.GetActiveScene(1);
+	}
+
+	public void Quit () {
+
+		Aplication.Quit();
+	}
+
+
